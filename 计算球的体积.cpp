657c3b895df54177981e2824879398efafe5be23
akalils <98333287+akalils@users.noreply.github.com>
2024-05-10 22:06:06 +0800

    Create 计算球的体积.cpp

---
+++ 计算球的体积.cpp
@@ -0,0 +1,14 @@
+# 根据输入的半径值，计算球的体积。
+#define PI 3.1415927
+#include <iostream>
+using namespace std;
+int main()
+{
+    double r;
+    while (cin >> r)
+    {
+        double V = (4 * PI * r * r * r) / 3;
+        printf("%.3lf", V);
+    }
+    return 0;
+}
